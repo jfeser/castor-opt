1c9112512e9a57246aa8e0fa9da089a781abc9b4
Jack Feser <feser@csail.mit.edu>
2020-04-07 15:38:15 -0400

    Fix ambiguous name error in elim-filter-simple.

--- lib/filter_tactics.ml
+++ lib/filter_tactics.ml
@@ -278,12 +278,17 @@ module Make (C : Config.S) = struct
     let scope = Fresh.name Global.fresh "s%d" in
     let%bind values = Tactics_util.all_values_attr attr in
     let sattr = Name.scoped scope attr in
+    let select_list =
+      Schema.schema r
+      |> List.filter ~f:(fun n -> Name.O.(n <> attr))
+      |> Schema.to_select_list
+    in
     return
     @@ A.list values scope
          (A.tuple
             [
               A.filter p @@ A.scalar (Name sattr);
-              A.filter P.(Name attr = Name sattr) r;
+              A.select select_list @@ A.filter P.(Name attr = Name sattr) r;
             ]
             Cross)
 
